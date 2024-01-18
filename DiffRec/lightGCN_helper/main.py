505b1d8806f60f77298e4f5b8f1b9135f4400ca2
dangkh <dangkh109@gmail.com>
2024-01-18 08:37:47 +0700

    add

---
+++ DiffRec/lightGCN_helper/main.py
@@ -0,0 +1,18 @@
+import world
+import utils
+from world import cprint
+import torch
+import numpy as np
+import time
+import Procedure
+from os.path import join
+# ==============================
+utils.set_seed(world.seed)
+print(">>SEED:", world.seed)
+# ==============================
+import register
+from register import dataset
+
+Recmodel = register.MODELS[world.model_name](world.config, dataset)
+Recmodel = Recmodel.to(world.device)
+bpr = utils.BPRLoss(Recmodel, world.config)
---
+++ DiffRec/lightGCN_helper/register.py
@@ -0,0 +1,28 @@
+import world
+import dataloader
+import model
+import utils
+from pprint import pprint
+import os
+print(os.getcwd())
+stop
+if world.dataset in ['baby', 'sport', 'cloth']:
+    dataset = dataloader.Loader(path="../../data/"+world.dataset)
+elif world.dataset == 'lastfm':
+    dataset = dataloader.LastFM()
+
+print('===========config================')
+pprint(world.config)
+print("cores for test:", world.CORES)
+print("comment:", world.comment)
+print("tensorboard:", world.tensorboard)
+print("LOAD:", world.LOAD)
+print("Weight path:", world.PATH)
+print("Test Topks:", world.topks)
+print("using bpr loss")
+print('===========end===================')
+
+MODELS = {
+    'mf': model.PureMF,
+    'lgn': model.LightGCN
+}
