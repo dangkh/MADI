191a1bea3678656b2c8b5255e317ff9e0c5b5ec9
dangkh <dangkh109@gmail.com>
2024-01-21 14:34:31 +0700

    update

--- DiffRec/LDiffModels/DNN.py
+++ DiffRec/LDiffModels/DNN.py
@@ -109,3 +109,81 @@ def xavier_normal_initialization(module):
             constant_(module.bias.data, 0)         
 
 
+class DNN_signal(nn.Module):
+    """
+    A deep neural network for the reverse diffusion preocess.
+    """
+    def __init__(self, in_dims, out_dims, emb_size, time_type="cat", norm=False, dropout=0.5):
+        super(DNN_signal, self).__init__()
+        self.in_dims = in_dims
+        self.out_dims = out_dims
+        assert out_dims[0] == in_dims[-1], "In and out dimensions must equal to each other."
+        self.time_type = time_type
+        self.time_emb_dim = emb_size
+        self.norm = norm
+
+        self.emb_layer = nn.Linear(self.time_emb_dim, self.time_emb_dim)
+        self.guide_layer = nn.Linear(self.in_dims[0], 16)
+
+        if self.time_type == "cat":
+            in_dims_temp = [self.in_dims[0] + self.time_emb_dim] + self.in_dims[1:]
+        else:
+            raise ValueError("Unimplemented timestep embedding type %s" % self.time_type)
+        out_dims_temp = self.out_dims
+        
+        self.in_layers = nn.ModuleList([nn.Linear(d_in, d_out) \
+            for d_in, d_out in zip(in_dims_temp[:-1], in_dims_temp[1:])])
+        self.out_layers = nn.ModuleList([nn.Linear(d_in, d_out) \
+            for d_in, d_out in zip(out_dims_temp[:-1], out_dims_temp[1:])])
+        
+        self.drop = nn.Dropout(dropout)
+        self.init_weights()
+    
+    def init_weights(self):
+        for layer in self.in_layers:
+            # Xavier Initialization for weights
+            size = layer.weight.size()
+            fan_out = size[0]
+            fan_in = size[1]
+            std = np.sqrt(2.0 / (fan_in + fan_out))
+            layer.weight.data.normal_(0.0, std)
+
+            # Normal Initialization for weights
+            layer.bias.data.normal_(0.0, 0.001)
+        
+        for layer in self.out_layers:
+            # Xavier Initialization for weights
+            size = layer.weight.size()
+            fan_out = size[0]
+            fan_in = size[1]
+            std = np.sqrt(2.0 / (fan_in + fan_out))
+            layer.weight.data.normal_(0.0, std)
+
+            # Normal Initialization for weights
+            layer.bias.data.normal_(0.0, 0.001)
+        
+        size = self.emb_layer.weight.size()
+        fan_out = size[0]
+        fan_in = size[1]
+        std = np.sqrt(2.0 / (fan_in + fan_out))
+        self.emb_layer.weight.data.normal_(0.0, std)
+        self.emb_layer.bias.data.normal_(0.0, 0.001)
+    
+    def forward(self, x, timesteps, maskInfo):
+        time_emb = timestep_embedding(timesteps, self.time_emb_dim).to(x.device)
+        # guide_emb = self.guide_layer(maskInfo) * 1e-3
+        emb = self.emb_layer(time_emb)
+        if self.norm:
+            x = F.normalize(x)
+        x = self.drop(x)
+        h = torch.cat([x+maskInfo, emb], dim=-1)
+        for i, layer in enumerate(self.in_layers):
+            h = layer(h)
+            h = torch.tanh(h)
+        
+        for i, layer in enumerate(self.out_layers):
+            h = layer(h)
+            if i != len(self.out_layers) - 1:
+                h = torch.tanh(h)
+        
+        return h
